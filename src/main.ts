15be4b5c27224ec65022a19fde7efe11abc843a9
mantisco-kangjuhyup <fog0510@gmail.com>
2023-06-10 20:54:18 +0900

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin : 'http://localhost:3000',
+    allowedHeaders: ['content-type'],
+    credentials : true
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +19,6 @@ async function bootstrap() {
   );
 
   
-  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
